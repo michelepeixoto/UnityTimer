a65d470526b8ba8b711f64824bad02e5a711d4ac
Michele Peixoto <michelepeixoto7@hotmail.com>
2021-05-17 00:29:27 -0700

    Add Pause function
    Adds a function to pause the timer if the Escape key is pressed.

--- Timer.cs
+++ Timer.cs
@@ -6,29 +6,59 @@ public class Timer : MonoBehaviour
 {
     [SerializeField] //allows private var to be editable in Inspector 
     private int _timerSeconds = 20;
+    private bool _timerPaused;
+    private bool _timerGoing;
     
     void Start()
     {
+        _timerPaused = false;
+        _timerGoing = true;
         StartCoroutine(timer(_timerSeconds));
+    }
+    
+    void Update()
+    {
+        if (_timerGoing)
+        {
+            if (Input.GetKeyDown(KeyCode.Escape))
+            {
+                PauseTimer();
+            }
+        }
     }
     
     IEnumerator timer(int timerSeconds)
     {
         while (true)
         {
-            while (_gamePaused)
+            while (_timerPaused)
             {
                 yield return null;
             }
-            //update timer UI
+            //implement code to update timer UI here
             Debug.Log("timer 0:" + timerSeconds);
             if (timerSeconds == 0)
             {
-                EndGame();
                 break;
             }
             timerSeconds -= 1;
             yield return new WaitForSeconds(1.0f);
+        }
+    }
+
+    private void PauseTimer()
+    {
+        if (_timerPaused == false)
+        {
+            Debug.Log("timer paused");
+            _timerPaused = true;
+            //implement code to show pause screen here
+        }
+        else
+        {
+            Debug.Log("timer unpaused");
+            _timerPaused = false;
+            //implement code to hide pause screen here
         }
     }
 }
